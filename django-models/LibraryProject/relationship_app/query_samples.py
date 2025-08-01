bc080800b58970f0c7f33b439c177c3f2aacff91
Alpatson Cobbina Siaw <cobbinaalpatson@gmail.com>
2025-08-01 18:34:48 +0000

    Initial commit: django-models with relationship_app

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,7 +2,7 @@
 
 from relationship_app.models import Author, Book, Library, Librarian
 
-# 1. Query all books by a specific author (using filter)
+# 1. Query all books by a specific author
 def get_books_by_author(author_name):
     try:
         author = Author.objects.get(name=author_name)
@@ -10,7 +10,7 @@ def get_books_by_author(author_name):
     except Author.DoesNotExist:
         return []
 
-# 2. List all books in a library (still valid)
+# 2. List all books in a library
 def get_books_in_library(library_name):
     try:
         library = Library.objects.get(name=library_name)
@@ -18,10 +18,10 @@ def get_books_in_library(library_name):
     except Library.DoesNotExist:
         return []
 
-# 3. Retrieve the librarian for a library
+# 3. Retrieve the librarian for a library (using expected pattern)
 def get_librarian_for_library(library_name):
     try:
         library = Library.objects.get(name=library_name)
-        return library.librarian  # still a OneToOneField
+        return Librarian.objects.get(library=library)
     except (Library.DoesNotExist, Librarian.DoesNotExist):
         return None
