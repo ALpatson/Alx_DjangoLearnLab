08d03a336d6b922f8cbebd0a041bd07b18203234
Alpatson Cobbina Siaw <cobbinaalpatson@gmail.com>
2025-08-02 08:55:59 +0000

    I implemented user roles and permissions

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -8,6 +8,11 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
 from django.shortcuts import render, redirect
 
+
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render
+from .models import UserProfile
+
 # Function-Based View
 def list_books(request):
     books = Book.objects.all()
@@ -48,3 +53,30 @@ def login_view(request):
 # def logout_view(request):
 #     logout(request)
 #     return render(request, 'relationship_app/logout.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # or wherever you want
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+def check_role(role):
+    def inner(user):
+        return hasattr(user, 'userprofile') and user.userprofile.role == role
+    return inner
+
+@user_passes_test(check_role('Admin'))
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(check_role('Librarian'))
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(check_role('Member'))
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
