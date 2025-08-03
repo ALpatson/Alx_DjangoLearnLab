c027afeee956830e7835ba06859201a3b0020cc7
Alpatson Cobbina Siaw <cobbinaalpatson@gmail.com>
2025-08-03 14:16:47 +0000

    Added Some security measures

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -3,3 +3,18 @@ from django import forms
 class BookSearchForm(forms.Form):
     q = forms.CharField(required=False, max_length=100)
 # [SECURITY] Use ORM filters to prevent SQL injection
+
+class ExampleForm(forms.Form):
+    name = forms.CharField(
+        max_length=100,
+        required=True,
+        widget=forms.TextInput(attrs={'placeholder': 'Enter your name'})
+    )
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(attrs={'placeholder': 'Enter your email'})
+    )
+    message = forms.CharField(
+        widget=forms.Textarea(attrs={'placeholder': 'Your message'}),
+        required=True
+    )
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from .models import Book
+from .forms import ExampleForm
 
 from django.db.models import Q
 
@@ -9,3 +10,17 @@ def book_list(request):
     books = Book.objects.filter(Q(title__icontains=query) | Q(author__icontains=query)) if query else Book.objects.all()
     return render(request, 'bookshelf/book_list.html', {'books': books, 'query': query})
 # [SECURITY] Use ORM filters to prevent SQL injection
+
+
+def example_form_view(request):
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            # Securely process form.cleaned_data here
+            name = form.cleaned_data['name']
+            return render(request, 'bookshelf/form_example.html', {
+                'form': ExampleForm(), 'success': True, 'name': name
+            })
+    else:
+        form = ExampleForm()
+    return render(request, 'bookshelf/form_example.html', {'form': form})
